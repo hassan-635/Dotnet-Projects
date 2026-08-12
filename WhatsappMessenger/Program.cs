ba7bb5e43d7a999d2364dfd9a9a28bc35efd96aa
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-08-12 10:40:28 +0500

    db connection added in program.cs

--- WhatsappMessenger/Program.cs
+++ WhatsappMessenger/Program.cs
@@ -1,8 +1,11 @@
+using WhatsappMessenger.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<DbConnection>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
