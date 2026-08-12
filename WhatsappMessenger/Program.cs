ceb745fae0772dbd1dea15d7dfbdd228ce009882
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-08-12 10:19:04 +0500

    folders update

--- WhatsappMessenger/Program.cs
+++ WhatsappMessenger/Program.cs
@@ -12,6 +12,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "WhatsApp Messenger API v1");
+    });
 }
 
 app.UseHttpsRedirection();
