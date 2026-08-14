45f37d0d845234d8eaf0ac634084622476327246
hassan-635 <hassanalimughal635@gmail.com>
2026-08-13 21:56:29 -0700

    add student and sow all students

--- StudentManagementSystem/Student Management System/Student Management System/Services/StudentManager.cs
+++ StudentManagementSystem/Student Management System/Student Management System/Services/StudentManager.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Student_Management_System.Interfaces;
+using Student_Management_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Student_Management_System.Services
 {
-    internal class StudentManager
+    internal class StudentManager : IStudentManager
     {
+        private List<Student> students = new List<Student>();
+
+        public void addStudent(Student student)
+        {
+                if(students.Any(existingStudent => existingStudent.studentId == student.studentId))
+                {
+                    Console.WriteLine("Student Already Exist.. Please Enter Unique Student Id...");
+                }
+                else
+                {
+                    students.Add(student);
+                }
+            
+                Console.WriteLine("Student Added Successfully....");
+        }
+
+        public List<Student> showAllStudents()
+        {
+            return students;
+        }
     }
 }
