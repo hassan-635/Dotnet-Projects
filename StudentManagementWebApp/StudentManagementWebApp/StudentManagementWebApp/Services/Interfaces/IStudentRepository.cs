60039fa8dccf015b3f6fe84515325532e1fb8394
hassan-635 <hassanalimughal635@gmail.com>
2026-08-14 23:15:52 -0700

    student repository interface

---
+++ StudentManagementWebApp/StudentManagementWebApp/StudentManagementWebApp/Services/Interfaces/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using StudentManagementWebApp.Models;
+
+namespace StudentManagementWebApp.Services.Interfaces
+{
+    public interface IStudentRepository
+    {
+
+        public IEnumerable<Student> GetAll();
+        public Student? GetById(string StudentId);
+        public void Add(Student student);
+        public bool Update(Student student);
+        public bool Delete(string StudentId);
+    }
+}
