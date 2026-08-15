8e01b6a65f47b576c2ba9105b5560137f315b83f
hassan-635 <hassanalimughal635@gmail.com>
2026-08-15 00:14:19 -0700

    student service interface

---
+++ StudentManagementWebApp/StudentManagementWebApp/StudentManagementWebApp/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using StudentManagementWebApp.Models;
+
+namespace StudentManagementWebApp.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        public IEnumerable<Student> GetAllStudents();
+        public Student? GetStudentById(string StudentId);
+        public bool CreateStudent(Student student);
+        public bool UpdateStudent(Student student);
+        public bool DeleteStudent(string StudentId);
+    }
+}
