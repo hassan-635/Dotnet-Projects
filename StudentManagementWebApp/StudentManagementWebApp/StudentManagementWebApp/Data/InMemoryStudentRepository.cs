5b504c35dd0ec649c61ba5a96ef2929f1e563abc
hassan-635 <hassanalimughal635@gmail.com>
2026-08-15 00:09:54 -0700

    studebnt repo methods

---
+++ StudentManagementWebApp/StudentManagementWebApp/StudentManagementWebApp/Data/InMemoryStudentRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StudentManagementWebApp.Services.Interfaces;
+using StudentManagementWebApp.Models;
+
+namespace StudentManagementWebApp.Data
+{
+    public class InMemoryStudentRepository : IStudentRepository
+    {
+        private List<Student> students= new List<Student>();
+        public IEnumerable<Student> GetAll()
+        {
+            return students;
+        }
+
+        public Student? GetById(string StudentId)
+        {
+            Student student = students.FirstOrDefault(s => s.StudentId == StudentId);
+
+            if(student == null)
+            {
+                return null;
+            }
+            return student;
+        }
+
+        public void Add(Student student)
+        {
+            students.Add(student);
+        }
+
+        public bool Update(Student student)
+        {
+            var ExistingStudent = students.FirstOrDefault(s => s.StudentId == student.StudentId);
+            if(ExistingStudent == null)
+            {
+                return false;
+            }
+            ExistingStudent.Name = student.Name;
+            ExistingStudent.Age = student.Age;
+            ExistingStudent.Email = student.Email;
+            ExistingStudent.Course = student.Course;
+            ExistingStudent.Semester = student.Semester;
+            ExistingStudent.Marks = student.Marks;
+            ExistingStudent.AttendancePercentage = student.AttendancePercentage;
+
+            return true;
+        }
+
+        public bool Delete(string StudentId)
+        {
+            var ExistingStudent = students.FirstOrDefault(s => s.StudentId == StudentId);
+            if(ExistingStudent == null)
+            {
+                return false;
+            }
+            students.Remove(ExistingStudent);
+            return true;
+        }
+
+    }
+}
