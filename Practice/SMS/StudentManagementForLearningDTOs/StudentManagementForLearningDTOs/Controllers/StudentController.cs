a599363d31032d875ebc564211c40cead09de119
hassan-635 <hassanalimughal635@gmail.com>
2026-08-18 09:45:11 -0700

    stuident management system structure for learning dtos

---
+++ Practice/SMS/StudentManagementForLearningDTOs/StudentManagementForLearningDTOs/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementForLearningDTOs.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAllStudents()
+        {
+            return View();
+        }
+
+        public IActionResult ViewStudentDetails()
+        {
+            return View();
+        }
+
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        public IActionResult EditStudent()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteStudent()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice/SMS/StudentManagementForLearningDTOs/StudentManagementForLearningDTOs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentManagementForLearningDTOs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
