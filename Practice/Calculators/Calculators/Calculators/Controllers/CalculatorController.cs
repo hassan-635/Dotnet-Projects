71ab43c2973f1f212d7fb72f1c4f802d052d8429
hassan-635 <hassanalimughal635@gmail.com>
2026-08-18 02:08:46 -0700

    controller and views for them

---
+++ Practice/Calculators/Calculators/Calculators/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculators.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Basic()
+        {
+            return View();
+        }
+
+        public IActionResult BMI()
+        {
+            return View();
+        }
+
+        public IActionResult Loan()
+        {
+            return View();
+        }
+    }
+}
