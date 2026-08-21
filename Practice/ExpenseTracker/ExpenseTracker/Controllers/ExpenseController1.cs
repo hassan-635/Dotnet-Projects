0293c415e5c4605f2bbd5323e261bf577ea8b276
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 10:12:34 -0700

    project structure for expense tracker along with expense controller and model

---
+++ Practice/ExpenseTracker/ExpenseTracker/Controllers/ExpenseController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpenseController1 : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice/ExpenseTracker/ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,6 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+    }
+}
