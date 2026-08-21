b7e7655b4cc1d95bb0285f6ff02ede39825173e7
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 05:05:59 -0700

    book services all methods

---
+++ Practice/BMS/BookManagement/BookManagement/Services/BookService.cs
@@ -0,0 +1,34 @@
+﻿using BookManagement.DTOs;
+
+namespace BookManagement.Services
+{
+    public class BookService
+    {
+        public List<BookDto> books = new List<BookDto>();
+        public List<BookDto> GetAll()
+        {
+            return books;
+        }
+
+        public BookDto? GetById(int Id)
+        {
+            BookDto book = new BookDto();
+            return book;
+        }
+
+        public void Add(BookDto dto)
+        {
+
+        }
+
+        public void Update(BookDto dto)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
