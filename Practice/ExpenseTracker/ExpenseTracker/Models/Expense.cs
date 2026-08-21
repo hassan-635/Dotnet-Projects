3240acab5da92abd4ecd4fe76ad68185cba063dc
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 10:18:15 -0700

    data annotations for expense model

--- Practice/ExpenseTracker/ExpenseTracker/Models/Expense.cs
+++ Practice/ExpenseTracker/ExpenseTracker/Models/Expense.cs
@@ -1,11 +1,21 @@
-﻿namespace ExpenseTracker.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
 {
+    [Index(nameof(Id), IsUnique = true)]
     public class Expense
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Title { get; set; }
+        [Required]
         public int Amount { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public DateOnly Date { get; set; }
     }
 }
