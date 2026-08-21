c8f0280ce82c270f65894044b71872da80670a4a
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 04:44:05 -0700

    book dto

---
+++ Practice/BMS/BookManagement/BookManagement/DTOs/BookDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookManagement.DTOs
+{
+    public class BookDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Author { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
