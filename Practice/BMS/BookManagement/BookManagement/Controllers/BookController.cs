6ada03f404af25a770685f20b39d468a79730686
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 04:42:28 -0700

    book management system structure to learn services and controllers along with usage of DTROs

---
+++ Practice/BMS/BookManagement/BookManagement/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagement.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice/BMS/BookManagement/BookManagement/Models/BookModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Models
+{
+    [Index(nameof(Id), IsUnique = true)]
+    public class BookModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Author { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
