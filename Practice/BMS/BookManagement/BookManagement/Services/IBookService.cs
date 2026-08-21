ba14cbfacd110473aa5b0bc0ebed841e5a6b2f78
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 04:48:15 -0700

    interface for book services

---
+++ Practice/BMS/BookManagement/BookManagement/Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using BookManagement.DTOs;
+
+namespace BookManagement.Services
+{
+    public interface IBookService
+    {
+        public List<BookDto> GetAll();
+
+        public BookDto? GetById(int id);
+        public void Add(BookDto dto);
+        public void Update(BookDto dto);
+        public void Delete(int id);
+    }
+}
