51782140a154b5ee584e3a07e17f78eb71870fab
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 10:17:19 -0700

    data annotations for expense dto

--- Practice/ExpenseTracker/ExpenseTracker/DTOs/ExpenseDTO.cs
+++ Practice/ExpenseTracker/ExpenseTracker/DTOs/ExpenseDTO.cs
@@ -1,11 +1,21 @@
-﻿namespace ExpenseTracker.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.DTOs
 {
+    [Index(nameof(Id), IsUnique = true)]
     public class ExpenseDTO
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Title { get; set; }
+        [Required]
         public int Amount { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public DateOnly Date { get; set; }
     }
 }
