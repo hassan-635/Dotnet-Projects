30fd0c46928f7976e7e6ef6965f6dcd30ccbfe12
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 10:20:12 -0700

    delete expense dto

---
+++ Practice/ExpenseTracker/ExpenseTracker/DTOs/DeleteExpenseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.DTOs
+{
+    public class DeleteExpenseDto
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
