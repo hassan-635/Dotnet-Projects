c85d8e0cd64c77d4c808f29955a6966a6cebc281
hassan-635 <hassanalimughal635@gmail.com>
2026-08-22 05:51:38 -0700

    project structure

---
+++ Practice/MyNotes/Controllers/NotesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyNotes.Controllers
+{
+    public class NotesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice/MyNotes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyNotes.Models
+{
+    public class Note
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(18)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+    }
+}
