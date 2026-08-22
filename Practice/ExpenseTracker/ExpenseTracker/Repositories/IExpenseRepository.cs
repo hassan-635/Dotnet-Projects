948c83c6b99833d0f5e7d75cffe1af006023648e
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 22:52:22 -0700

    expense repository interface methods

--- Practice/ExpenseTracker/ExpenseTracker/Repositories/IExpenseRepository.cs
+++ Practice/ExpenseTracker/ExpenseTracker/Repositories/IExpenseRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ExpenseTracker.Repositories
+﻿using ExpenseTracker.DTOs;
+
+namespace ExpenseTracker.Repositories
 {
     public interface IExpenseRepository
     {
+        public List<ExpenseDTO> GetAll();
+        public ExpenseDTO GetById(int Id);
+        public bool Add(ExpenseDTO Expense);
+        public bool Update(ExpenseDTO Expense);
+        public bool Delete(int Id);
     }
 }
