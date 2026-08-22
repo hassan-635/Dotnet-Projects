92a2ed6d80bcd14485c97e715eb3d57ef557877e
hassan-635 <hassanalimughal635@gmail.com>
2026-08-21 23:05:59 -0700

    expense service interface methods

--- Practice/ExpenseTracker/ExpenseTracker/Services/IExpenseService.cs
+++ Practice/ExpenseTracker/ExpenseTracker/Services/IExpenseService.cs
@@ -1,6 +1,15 @@
-﻿namespace ExpenseTracker.Services
+﻿using ExpenseTracker.DTOs;
+
+namespace ExpenseTracker.Services
 {
     public interface IExpenseService
     {
+        public List<ExpenseDTO> GetAllExpenses();
+        public ExpenseDTO GetExpenseById(int Id);
+        public bool AddExpense(ExpenseDTO Expense);
+        public bool UpdateExpense(ExpenseDTO Expense);
+        public bool DeleteExpense(int Id);
+        public decimal CalculateTotal();
+        public decimal GetCategoryTotal(string category);
     }
 }
